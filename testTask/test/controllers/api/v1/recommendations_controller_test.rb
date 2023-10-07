d6d02a1fadc3994b77fccfb7e28785f93b320bd3
AZRV17 <alex170805azrov@gmail.com>
2023-10-07 18:26:46 +0300

    Create tests for RecommendationsController

--- testTask/test/controllers/api/v1/recommendations_controller_test.rb
+++ testTask/test/controllers/api/v1/recommendations_controller_test.rb
@@ -1,41 +1,13 @@
 require "test_helper"
 
 class Api::V1::RecommendationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get api_v1_recommendations_create_url
+  test "should post recommendation" do
+    post "/api/v1/consultation_requests/980190962/recommendations"
+    assert_response :success
+  end
+
+  test "should get recommendations" do
+    get "/api/v1/patients/980190962/recommendations"
     assert_response :success
   end
 end
-
-# RSpec.describe RecommendationsController, type: :controller do
-#   let(:consultation_request) { create(:consultation_request) }
-#
-#   describe '#create' do
-#     it 'creates a new recommendation' do
-#       post :create, params: {
-#         id: consultation_request.id,
-#         text: 'This is a recommendation.'
-#       }
-#
-#       expect(response).to have_http_status(:created)
-#       expect(response.body).to include('Recommendation created successfully')
-#
-#       recommendation = Recommendation.find_by(consultation_request_id: consultation_request.id)
-#       expect(recommendation).to be_present
-#       expect(recommendation.text).to eq('This is a recommendation.')
-#     end
-#
-#     it 'returns errors if the recommendation cannot be created' do
-#       post :create, params: {
-#         id: consultation_request.id,
-#         text: ''
-#       }
-#
-#       expect(response).to have_http_status(:bad_request)
-#       expect(response.body).to include('Text cannot be blank')
-#
-#       recommendation = Recommendation.find_by(consultation_request_id: consultation_request.id)
-#       expect(recommendation).to be_nil
-#     end
-#   end
-# end
